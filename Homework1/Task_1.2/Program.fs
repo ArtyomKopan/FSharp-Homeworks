cc45848d68fe47dba1a691678fb5af8ec484e511
ArtyomKopan <artem.kopan.2003@mail.ru>
2023-03-02 23:21:08 +0300

    Refactored task 1.2

--- Homework1/Task_1.2/Program.fs
+++ Homework1/Task_1.2/Program.fs
@@ -1,6 +1,9 @@
-﻿let rec fib n =
+﻿let fibonacci n =
+    let rec fib n current_num f1 f2 =
+        if current_num = n then f1 + f2
+        else fib n (current_num + 1) f2 (f1 + f2)
     if n = 0 then 0
     else if n = 1 then 1
-    else fib (n - 1) + fib (n - 2)
+    else fib n 2 0 1
     
-printfn $"{fib 8}"
+printfn $"%A{List.map fibonacci [1..20]}"
