41cc2efa82c04eb1d31e53c1d72715efadda837d
ArtyomKopan <artem.kopan.2003@mail.ru>
2023-04-28 12:30:32 +0300

    Added a solution of task 1

---
+++ Test1/TestTask_1.1/Program.fs
@@ -0,0 +1,13 @@
+﻿module Supermap
+
+let supermap (functions: ('a -> 'b) list) (list: 'a list) : 'b list =
+    let rec apply_k_func (functions: ('a -> 'b) list) (list: 'a list) k current_results =
+        let k_result = List.map (fun f -> f list[k]) functions
+        match k with
+        | 0 -> k_result :: current_results
+        | _ -> apply_k_func functions list (k - 1) (k_result :: current_results)
+                
+    let n = List.length functions
+    let result = apply_k_func functions list n []
+    List.concat result
+    
