c490d980b82b9c18a9a128ad9af898965c402fa8
ArtyomKopan <artem.kopan.2003@mail.ru>
2023-04-28 13:08:02 +0300

    Added tests for task 1

--- Test1/TestTask_1.1/Test.fs
+++ Test1/TestTask_1.1/Test.fs
@@ -8,3 +8,8 @@ let ``test 1``() =
    let l' = List.map (fun (x: float) -> System.Math.Round(x, 3)) l
    printfn $"%A{l'}"
    l' |> should equal (List.map (fun (x: float) -> System.Math.Round(x, 3)) [sin 1.0; cos 1.0; sin 2.0; cos 2.0; sin 3.0; cos 3.0])
+
+[<Test>]
+let ``test 2``() =
+   let l = supermap [(fun x -> x + 1); (fun x -> x * 2)] [1; 2; 3]
+   l |> should equal [2; 2; 3; 4; 4; 6]
