24a952cfbe4667ab57f7ecc4ab0f522f82ba8d52
ArtyomKopan <artem.kopan.2003@mail.ru>
2023-05-05 13:31:52 +0300

    change tests

--- Retest1/Task_3/Test.fs
+++ Retest1/Task_3/Test.fs
@@ -6,11 +6,14 @@ open FsUnit
 let ``test 1``() =
     evalMoves (0, 0) [] |> should equal (Some (0, 0))
 
+[<Test>]
 let ``test 2``() =
     evalMoves (0, 0) [Left 5] |> should equal None
     
+[<Test>]
 let ``test 3``() =
     evalMoves (0, 0) [Right 5; Top 10] |> should equal (Some (5, 10))
     
+[<Test>]
 let ``test 4``() =
     evalMoves (0, 0) [Right 5; Bottom 10] |> should equal None
