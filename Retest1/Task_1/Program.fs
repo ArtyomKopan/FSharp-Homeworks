e3d1d32206c9123648da5a8d02ea7a8deaa1aaf4
ArtyomKopan <artem.kopan.2003@mail.ru>
2023-05-05 13:11:04 +0300

    Added tasks 1 and 2 and tests

---
+++ Retest1/Task_1/Program.fs
@@ -0,0 +1,15 @@
+﻿module MapiRepeat
+
+exception InvalidArgumentException of string
+
+let mapiRepeat f n list =
+    if n < 0 then
+        failwith "Число применений функции должно быть положительным!"
+    
+    let rec applyMapi k f list =
+        match k with
+        | 0 -> list
+        | 1 -> List.mapi f list
+        | _ -> applyMapi (k - 1) f (List.mapi f list)
+    
+    applyMapi n f list
---
+++ Retest1/Task_2/Program.fs
@@ -0,0 +1,13 @@
+﻿module Drawing
+
+let drawX n =
+  let size = 2 * n - 1
+  String.init size (fun lineIndex ->
+    let k = min lineIndex (size - lineIndex - 1)
+    String.init size (fun columnIndex
+                       -> if columnIndex = k || columnIndex = size - 1 - k then "*"
+                          else " ") + "\n")
+  |> printf "%s"
+  
+// usage example
+drawX 5
