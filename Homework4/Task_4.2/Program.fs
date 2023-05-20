02435bdc7dcd621408d64d79230a96d7f3dc6cc7
ArtyomKopan <artem.kopan.2003@mail.ru>
2023-05-20 15:06:47 +0300

    Refactored task 4.2

--- Homework4/Task_4.2/Program.fs
+++ Homework4/Task_4.2/Program.fs
@@ -5,3 +5,5 @@ let func x l = List.map (fun y -> y * x) l
 let func' x = List.map (fun y -> y * x)
 
 let func'' x = List.map ((*) x)
+
+let func''' = (*) >> List.map
--- Homework4/Task_4.2/Test.fs
+++ Homework4/Task_4.2/Test.fs
@@ -2,10 +2,12 @@ open PointFree
 
 open FsCheck
 
-let pointFreeTest (list: int list) =
-    let list' = func 2 list
-    let list'' = (func' 2) list
-    let list''' = (func'' 2) list
-    list' = list'' && list' = list'' && list'' = list'''
+let pointFreeTest (l: int list) =
+    let list = func 2 l
+    let list' = func' 2 l
+    let list'' = func'' 2 l
+    let list''' = func''' 2 l  
+    list = list' && list = list'' && list = list''' &&
+        list' = list'' && list' = list''' && list'' = list'''
     
 Check.Quick pointFreeTest
