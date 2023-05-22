5b72a9416fe0eca2f7c121cde5a4809716e4c102
ArtyomKopan <artem.kopan.2003@mail.ru>
2023-05-23 01:01:48 +0300

    Changed a solutions of tasks 3.1, 3.2, 3.3

--- Homework3/Task_3.3/Program.fs
+++ Homework3/Task_3.3/Program.fs
@@ -1,5 +1,5 @@
 ﻿module Lambda
-
+ 
 type LambdaExpression =
     | Var of string
     | Abstraction of string * LambdaExpression
@@ -15,53 +15,53 @@ let generateVarName var = var + "'"
   
 let rec alphaConversion expression var newVar =
     match expression with
-    | Var v -> if v = var then Var(newVar) else Var(v)
+    | Var v -> Var (if v = var then newVar else v)
     | Application (expr1, expr2) -> Application (alphaConversion expr1 var newVar,
                            alphaConversion expr2 var newVar)
-    | Abstraction (v, e) -> if v = var then
-                                Abstraction (newVar, alphaConversion e v newVar)
-                            elif Set.contains newVar (getFreeVars e) then
-                                let v' = generateVarName v
-                                Abstraction (
-                                    v',
-                                    alphaConversion (alphaConversion (Abstraction (v', e)) v v') var newVar 
-                                )
-                            else Abstraction (v, alphaConversion e var newVar)
-
+    | Abstraction (v, e) -> 
+        if v = var then
+            Abstraction (newVar, alphaConversion e v newVar)
+        elif Set.contains newVar (getFreeVars e) then
+            let v' = generateVarName v
+            Abstraction (
+                v',
+                alphaConversion (alphaConversion (Abstraction (v', e)) v v') var newVar 
+            )
+        else Abstraction (v, alphaConversion e var newVar)
+ 
 let alphaRename expression var =
     if Set.contains var (getFreeVars expression) then
         let newVar = generateVarName var
         (alphaConversion expression var newVar, newVar)
     else (expression, var)
-
-let rec replaceTherm expression varName therm =
+ 
+let rec replaceTerm expression varName term =
     match expression with
-    | Var v when v = varName -> therm
+    | Var v when v = varName -> term
     | Var _ -> expression
-    | Abstraction (v, expr) when v = varName -> replaceTherm expr varName therm
+    | Abstraction (v, expr) when v = varName -> Abstraction (v, expr)
     | Abstraction (v, expr) ->
-        if (not (Set.contains v (getFreeVars therm)) || not (Set.contains varName (getFreeVars expr))) then
-            Abstraction (v, replaceTherm expr varName therm)
+        if (not (Set.contains v (getFreeVars term)) || not (Set.contains varName (getFreeVars expr))) then
+            let replacedExpr = replaceTerm expr varName term
+            Abstraction (v, replacedExpr)
         else
             let renamedExpr, v' = alphaRename expr v
-            Abstraction (v', replaceTherm renamedExpr varName therm)
+            let replacedExpr = replaceTerm renamedExpr varName term
+            Abstraction(v', replacedExpr)
     | Application (expr1, expr2) ->
-        Application(replaceTherm expr1 varName therm,
-                     replaceTherm expr2 varName therm)
+        Application(replaceTerm expr1 varName term, replaceTerm expr2 varName term)
         
 let rec normalize expression =
     match expression with
     | Var _ -> expression
     | Abstraction (v, expr) -> Abstraction (v, normalize expr)
-    | Application (Abstraction (v, expr1), expr2) -> betaConversion v expr1 expr2
-    | Application (expr1, expr2) -> Application (normalize expr1, normalize expr2)
-and
-  betaConversion v expression1 expression2 =
-      let newExpression1, v' = alphaRename expression1 v
-      normalize (replaceTherm newExpression1 v' expression2)
+    | Application (Abstraction (v, expr1), expr2) ->
+        normalize (replaceTerm expr1 v expr2)
+    | Application (expr1, expr2) ->
+        Application (normalize expr1, normalize expr2)
     
 let rec lambdaToString expression =
     match expression with
     | Var v -> v
-    | Abstraction (v, expr) -> $"λ{v}." + lambdaToString expr
-    | Application (expr1, expr2) -> "(" + lambdaToString expr1 + ") " + lambdaToString expr2 
+    | Abstraction (v, expr) -> $"λ{v}.{lambdaToString expr}"
+    | Application (expr1, expr2) -> $"{lambdaToString expr1} {lambdaToString expr2}"
